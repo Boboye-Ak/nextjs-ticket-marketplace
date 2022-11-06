555658675990b08319a7566e4446ab3d06529060
Boboye-Ak <boboye771@gmail.com>
2022-11-07 00:01:53 +0100

    wrote code to fetch data for use in event page

--- components/Event.js
+++ components/Event.js
@@ -21,6 +21,8 @@ const Event = ({ partyAddress }) => {
     const [cost, setCost] = useState("")
     const [totalSold, setTotalSold] = useState("")
     const [maxAttendees, setMaxAttendees] = useState("")
+    const [numTickets, setNumTickets] = useState("")
+    const [myTickets, setMyTickets] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
     //Web3 Functions
@@ -60,20 +62,71 @@ const Event = ({ partyAddress }) => {
         functionName: "balanceOf",
         params: { owner: account },
     })
+    const { runContractFunction: getOwnerOf } = useWeb3Contract()
 
     //Web2 Functions
-    const updateUI = async () => {}
+    const updateUI = async () => {
+        let myTicketsArray = []
+        let getOwnerOfOptions = {
+            abi: partyAbi,
+            contractAddress: partyAddress,
+            functionName: "ownerOf",
+            params: { tokenId: null },
+        }
+        const partyNameFromCall = await getName()
+        const totalSoldFromCall = (await getTotalSold())?.toString()
+        const maxAttendeesFromCall = (await getMaxAttendees())?.toString()
+        let numTicketsFromCall = (await getNumTickets())?.toString()
+        if (
+            typeof numTicketsFromCall != "undefined" &&
+            parseInt(numTicketsFromCall?.toString()) > 0 &&
+            parseInt(totalSoldFromCall) > 0
+        ) {
+            numTicketsFromCall = parseInt(numTicketsFromCall)
+            for (let n = 1; n <= parseInt(totalSoldFromCall); n++) {
+                console.log(`checking token ID ${n}`)
+                getOwnerOfOptions.params.tokenId = n
+                let owner = await getOwnerOf({ params: getOwnerOfOptions })
+                if (owner?.toLowerCase() == account?.toLowerCase()) {
+                    console.log(`token ID ${n} is yours`)
+                    myTicketsArray.push(n)
+                    console.log(myTicketsArray)
+                } else {
+                    console.log(`token ID ${n} is not yours`)
+                }
+                if (myTicketsArray.length >= parseInt(numTicketsFromCall)) {
+                    break
+                }
+            }
+        } else {
+            numTicketsFromCall = 0
+        }
+        console.log(myTicketsArray)
+        setNumTickets(numTicketsFromCall)
+        setMaxAttendees(maxAttendeesFromCall)
+        setTotalSold(totalSoldFromCall)
+        setPartyName(partyNameFromCall)
+        setMyTickets(myTicketsArray)
+    }
 
     //useEffects
     useEffect(() => {
         if (isWeb3Enabled) {
             updateUI()
         }
-    }, [isWeb3Enabled, chainIdHex, account, partyFactoryAddress])
+    }, [isWeb3Enabled, chainId, account])
     return (
         <div>
             {isLoading && <Loader />}
-            Event with address {partyAddress}
+            Event with address {partyAddress}. You have {numTickets} tickets to {partyName}.{" "}
+            {totalSold} out of the total {maxAttendees} have been sold.
+            <br />
+            You own tickets with IDs:
+            <div>
+                {myTickets.map((ticket, index) => {
+                    return <div key={index}>{ticket}</div>
+                })}
+            </div>
         </div>
     )
 }
